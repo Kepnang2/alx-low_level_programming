539476fd2aea5dc754d92ff802f6d611a9f9023a
Maxime <kepemafa@yahoo.fr>
2022-07-03 05:14:53 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - program that prints digit numbers of base 10 without using char
+ * Return: Always (Success)
+ */
+int main(void)
+{
+int n;
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+}
+putchar('\n');
+
+return (0);
+
+}
