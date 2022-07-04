796fca5d809360e57f2e4b8e7997d2377a2f54cf
Maxime <kepemafa@yahoo.fr>
2022-07-04 08:15:57 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - prints all possible two two-digits numbers
+ * Return: Always (Success)
+ */
+int main(void)
+{
+int c, i;
+c = i = '0';
+for (c = '0'; c <= '9'; c++)
+{
+for (i = '0'; i <= '9'; i++)
+{
+putchar(c);
+putchar(i);
+if ((c != '9') || (c == '9' && i != '9'))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+
+return (0);
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-This repository tries to solve the different tasks of 0x00 hello world
+This repository tries to solve the different tasks of 0x01. C - Variables, if, else, while
 
