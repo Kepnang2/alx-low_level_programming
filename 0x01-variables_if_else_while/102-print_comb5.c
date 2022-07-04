fed85b33d092f8ad4ddcbdb8eb86d5051175136c
Maxime <kepemafa@yahoo.fr>
2022-07-04 11:27:21 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,22 +6,22 @@
 int main(void)
 {
 int c, i;
-c = i = '0';
-for (c = '0'; c <= '9'; c++)
+for (c = 0; c <= 98; c++)
 {
-for (i = '0'; i <= '9'; i++)
+for (i = c + 1; i <= 99; i++)
 {
-putchar(c);
-putchar(i);
-if ((c != '9') || (c == '9' && i != '9'))
-{
+putchar((c / 10) + '0');
+putchar((c % 10) + '0');
+putchar(' ');
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
+if (c == 98 && i == 99)
+continue;
 putchar(',');
 putchar(' ');
 }
 }
-}
 putchar('\n');
-
 return (0);
 
 }
