1ce17f79da26f3e67ee07eef024d70e037c405b4
Maxime <kepemafa@yahoo.fr>
2022-07-11 11:28:42 -0700

    length of string done

--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -2,3 +2,5 @@
 98 Battery s.
 #Task 1
 Don't swap horses in crossing a stream
+#Task 2
+This report, by its very length, defends itself against the risk of being read
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,9 @@
 #ifndef main_H
 #define main_H
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-#endif /* main.h */
+int _strlen(char *s);
+
+#endif /* main_H */
