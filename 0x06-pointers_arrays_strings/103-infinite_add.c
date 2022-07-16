62b37441eae6b027f8b597faa3289ac108bcbed4
Maxime <kepemafa@yahoo.fr>
2022-07-15 18:24:31 -0700

    Task 11 done

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -15,30 +15,29 @@
  * @size_r: size of buffer
  * Return: returns pointer to result
  */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int num, tens = 0;
 
-	for (; *n1 && *n2; n1--, n2--, r_index--)
+	for (; *n1 && *n2; n1--, n2--, size_r--)
 	{
 		num = (*n1 - '0') + (*n2 - '0');
 		num += tens;
-		*(r + r_index) = (num % 10) + '0';
+		*(r + size_r) = (num % 10) + '0';
 		tens = num / 10;
 	}
 
-	for (; *n1; n1--; r_index--)
+	for (; *n1; n1--, size_r--)
 	{
 		num = *(n1 - '0') + tens;
-		*(r + r_index) = (num % 10) + '0';
+		*(r + size_r) = (num % 10) + '0';
 		tens = num / 10;
 	}
 
-	for (; *n2; n2--; r_index--)
+	for (; *n2; n2--, size_r--)
 	{
 		num = (*n2 - '0') + tens;
-		*(r + r_index) = (num % 10) + '0';
+		*(r + size_r) = (num % 10) + '0';
 		tens = num / 10;
 	}
 }
