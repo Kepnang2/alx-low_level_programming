3ef68dda66c2073e76d9beddfefb94e8f6b135c2
Maxime <kepemafa@yahoo.fr>
2022-08-01 07:22:54 -0700

    struct dog done

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _HEADER_
+#define _HEADER_
+
+/**
+ * struct dog - dog attributes
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: the attributes of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
