ad58011ccdf26a607afca5e6404caf2dc16e3b6b
Maxime <kepemafa@yahoo.fr>
2022-08-04 15:57:24 -0700

    function that prints numbers, followed by a new line

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
 #ifndef _HEADER_
 #define _HEADER_
-
+int _putchar(char c);
+void print_numbers(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
 #endif
