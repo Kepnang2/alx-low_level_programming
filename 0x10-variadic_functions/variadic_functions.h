1005bafb074e852568b6cd56a50c8f2922b70947
Maxime <kepemafa@yahoo.fr>
2022-08-04 17:34:03 -0700

    function that prints anything

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,23 @@
 #ifndef _HEADER_
 #define _HEADER_
+#include <stdarg.h>
+
 int _putchar(char c);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 int sum_them_all(const unsigned int n, ...);
+
+/**
+ * struct format - Data type of a format.
+ * @op: Format.
+ * @f: Function.
+ *
+ */
+
+typedef struct format
+{
+	char *op;
+	void (*f)(va_list all);
+} f;
 #endif
