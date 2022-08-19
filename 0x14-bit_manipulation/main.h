25d0e40ff87cbda8562847929e9493d59afee808
Maxime <kepemafa@yahoo.fr>
2022-08-18 19:13:00 -0700

    Task 5 done

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,6 +2,7 @@
 #define _MAIN_H
 
 #include <stddef.h>
+int clear_bit(unsigned long int *n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);
 void print_binary(unsigned long int n);
