7bb33e930573d9416d37f1d1a2765ba6c2eeb66c
Maxime <kepemafa@yahoo.fr>
2022-08-22 12:41:30 -0700

    Tread lightly, she is near done

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,16 +18,23 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	text = malloc(letters);
 	if (text == NULL)
 		return (0);
+
 	if (filename == NULL)
 		return (0);
+
 	file = open(filename, O_RDONLY);
+
 	if (file == -1)
 	{
 		free(text);
 		return (0);
 	}
+
 	let = read(file, text, letters);
+
 	w = write(STDOUT_FILENO, text, let);
+
 	close(file);
+
 	return (w);
 }
