8df2cd90a6fa757d6ed4d3eb5c6d61adaf30dbb3
Maxime <kepemafa@yahoo.fr>
2022-08-26 15:22:53 -0700

    Task 0 done

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,42 +3,32 @@
 #include "main.h"
 
 /**
- * read_textfile - that reads a text file and prints
- * @filename: variable pointer
- * @letter: size letters
- * Description: Write a function that reads a text file and prints it
- * to the posix standard output.
- * Return: the actual number of letters it could read and print, 0 otherwise
- */
+ * read_textfile - function to read a text file and print to stdout
+ * @filename: the name of the file
+ * @letters: the number of letters to be printed
+ *
+ * Return: actual number of letters that was printed
+ **/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file_desc = 0;
-	int ch_printed = 0;
-	int status_wr = 0;
-	char *buffer;
+ssize_t fdo, fdr, fdw;
+char *buf;
 
-	if (!filename)
-		return (0);
-
-	file_desc = open(filename, O_RDONLY);
-
-	if (file_desc == -1)
-	{
-		return (0);
-	}
-
-	buffer = malloc(letters * sizeof(char));
-
-	if (!buffer)
-		return (0);
-
-	ch_printed = read(file_desc, buffer, letters);
-	status_wr = write(STDOUT_FILENO, buffer, ch_printed);
-
-	if (status_wr < 0)
-		return (0);
-
-	free(buffer);
-	close(file_desc);
-	return (ch_printed);
+if (filename == NULL)
+return (0);
+fdo = open(filename, O_RDONLY);
+if (fdo == -1)
+return (0);
+buf = malloc(sizeof(char) * letters);
+if (buf == NULL)
+return (0);
+fdr = read(fdo, buf, letters);
+if (fdr == -1)
+return (0);
+fdw = write(STDOUT_FILENO, buf, fdr);
+if (fdw == -1)
+return (0);
+free(buf);
+close(fdo);
+return (fdw);
 }
